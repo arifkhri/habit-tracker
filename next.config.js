94d65bd13ba12be7ca7cb5950e34c3610034d1f8
arifakhri <arifakhri97@gmail.com>
2023-02-18 22:46:14 +0700

    Setup eslint, styleling, and bootstrap

--- next.config.js
+++ next.config.js
@@ -6,3 +6,23 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
   // config
 })
+
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  poweredByHeader: false,
+  trailingSlash: true,
+  swcMinify: true,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+  // webpack: (config, options) => {
+  //   config.plugins.push(new StylelintPlugin());
+  //   return config;
+  // }
+});
